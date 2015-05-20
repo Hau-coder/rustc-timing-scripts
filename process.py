60422f06c082742e3ae20adfe9c5b1b8e4b013d2
Nick Cameron <ncameron@mozilla.com>
2015-05-20 20:06:53 +1200

    process.py does the averaging of runs

--- process.py
+++ process.py
@@ -8,28 +8,33 @@ LL_RATIO = False
 
 re_commit = re.compile("commit (.*)")
 re_date = re.compile("Date:   (.*)")
-re_rustc = re.compile("rustc: .*/(\w*)")
+re_rustc = re.compile("rustc: .*/([\w\-_\.]*)")
 re_time = re.compile("( *)time: ([0-9\.]*)\s*(.*)")
 
 
 def process(label, arg, n):
+    in_files = []
     for i in range(0, n):
         in_name = os.path.join('raw', '%s--%s--%s.log'%(label, arg, i))
-        out_name = os.path.join('processed', '%s--%s--%s.json'%(label, arg, i))
-        if VERBOSE:
-            print "input:", in_name
-            print "output:", out_name
+        in_files.append(open(in_name))
 
-        with open(in_name) as in_file:
-            with open(out_name, 'w') as out_file:
-                process_file(in_file, out_file)
+    out_name = os.path.join('processed', '%s--%s.json'%(label, arg))
+    if VERBOSE:
+        print "input:", in_files
+        print "output:", out_name
+
+    with open(out_name, 'w') as out_file:
+        process_files(in_files, out_file)
+
+    for f in in_files:
+        f.close()
 
 
-
-def process_file(in_file, out_file):
+def process_files(in_files, out_file):
     data = {}
-    data['header'] = mk_header(in_file)
-    data['times'] = mk_times(in_file)
+    data['header'] = mk_header(in_files[0])
+    times = map(lambda f: mk_times(f), in_files)
+    data['times'] = map(post_process_times, merge_times(times))
 
     json.dump(data, out_file, indent=4)
 
@@ -71,7 +76,7 @@ def mk_times(in_file):
             if not indent:
                 cur_times['times'].append((label, float(time)))
         elif cur_times:
-            all_times.append(process_times(cur_times))
+            all_times.append(cur_times)
             cur_times = None
             last_file = None
 
@@ -81,8 +86,37 @@ def mk_times(in_file):
 
     return all_times
 
+# Takes an array of times and returns a single object of times,
+def merge_times(times):
+    for t in times:
+        t.sort(key=lambda t: t['crate'])
+        if len(t) != len(times[0]):
+            print "Inconsistent data"
+            return
 
-def process_times(times):
+    crates = []
+    for ci in range(len(times[0])):
+        c = times[0][ci]
+        cur = {}
+        cur['crate'] = c['crate']
+        cur['times'] = []
+        for i in range(len(c['times'])):
+            cur['times'].append((c['times'][i][0], average(times, lambda t: t[ci]['times'][i][1])))
+        crates.append(cur)
+
+    return crates
+
+def average(times, f):
+    if len(times) <= 4:
+        total = sum(map(f, times))
+        return total/len(times)
+
+    # Exclude the highest and lowest values.
+    times = map(f, times)
+    times.sort()
+    return sum(times[1:-1])/(len(times)-2)
+
+def post_process_times(times):
     total = 0
     llvm = 0
     for (l, t) in times['times']:
@@ -90,7 +124,6 @@ def process_times(times):
         if LL_RATIO and l in ['translation', 'LLVM passes', 'linking']:
             llvm += t
 
-    times['total'] = total
     new_times = {}
 
     for (l, t) in times['times']:
@@ -104,6 +137,7 @@ def process_times(times):
         new_times[l] = time
 
     times['times'] = new_times
+    times['total'] = total
     return times
 
 
